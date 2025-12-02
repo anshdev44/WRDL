cba28225009837e10bd1ac636a02262289de068f
PIYUSH-SATIYA <pbsatiya007@gmail.com>
2025-12-02 22:57:15 +0530

    properly handled duplicate user joining hence removed false log

--- app/action/room.js
+++ app/action/room.js
@@ -1,125 +1,129 @@
-"use server"
+"use server";
 import Room from "../models/room";
 import connectDB from "../db/connect";
 import { fetchuser } from "./interaction";
 import { fetchprofilepic } from "./interaction";
 import { io } from "socket.io-client";
 
-
 // const sid=0;
 
-
 export const CreateNewRoom = async (email, username) => {
-  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-  const length = 6;
-  await connectDB();
-  for (let j = 0; j < 1000; j++) {
-    let roomID = "";
-    for (let i = 0; i < length; i++) {
-      roomID += chars.charAt(Math.floor(Math.random() * chars.length));
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const length = 6;
+    await connectDB();
+    for (let j = 0; j < 1000; j++) {
+        let roomID = "";
+        for (let i = 0; i < length; i++) {
+            roomID += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        const existingroom = await Room.findOne({ roomID: roomID });
+        const res = await fetchprofilepic(email);
+
+        // console.log("Fetched user for room creation:", res.profilepic);
+        if (!existingroom) {
+            await Room.create({
+                roomID,
+                players: [
+                    {
+                        username,
+                        profilepic: res.profilepic,
+                        email,
+                        role: "host",
+                    },
+                ],
+            });
+            // console.log("Room id", roomID);
+            return roomID;
+        }
     }
-    const existingroom = await Room.findOne({ roomID: roomID });
-    const res = await fetchprofilepic(email);
-
-    // console.log("Fetched user for room creation:", res.profilepic);
-    if (!existingroom) {
-      await Room.create({
-        roomID,
-        players: [{
-          username,
-          profilepic: res.profilepic,
-          email,
-          role: "host"
-        }]
-      });
-      // console.log("Room id", roomID);
-      return roomID;
-    }
-  }
-  return null;
-}
-
-
+    return null;
+};
 
 //increment player count
 export const incrementplayercount = async (roomID) => {
-  await connectDB();
-  const room = await Room.findOne({ roomID: roomID });
-  if (room) {
-    room.playercount = (room.playercount || 0) + 1;
-    await room.save();
-    return { message: "Player count incremented", status: 200 };
-  }
-  else {
-    return { error: "Room not found", status: 404 };
-  }
-
-}
+    await connectDB();
+    const room = await Room.findOne({ roomID: roomID });
+    if (room) {
+        room.playercount = (room.playercount || 0) + 1;
+        await room.save();
+        return { message: "Player count incremented", status: 200 };
+    } else {
+        return { error: "Room not found", status: 404 };
+    }
+};
 
 export const getroomdata = async (roomid) => {
-  await connectDB();
-  const room = await Room.findOne({ roomID: roomid }).lean();
-  if (room) {
-    room._id = String(room._id);
-    return { message: "Room data found", status: 200, room };
-  } else {
-    return { error: "Room not found", status: 404 };
-  }
-}
+    await connectDB();
+    const room = await Room.findOne({ roomID: roomid }).lean();
+    if (room) {
+        room._id = String(room._id);
+        return { message: "Room data found", status: 200, room };
+    } else {
+        return { error: "Room not found", status: 404 };
+    }
+};
 
 export const handlejoinroombackend = async (roomid, email, username) => {
-  await connectDB();
-  const exsistingroom = await Room.findOne({ roomID: roomid });
-  // console.log("Existing room:", exsistingroom);
-  if (!exsistingroom) {
-    return { error: "Room does not exsits", status: 404 };
-  }
-  else {
-    if (exsistingroom.players.length >= 4) {
-      return { error: "Room is full", status: 403 };
+    await connectDB();
+    const exsistingroom = await Room.findOne({ roomID: roomid });
+    // console.log("Existing room:", exsistingroom);
+    if (!exsistingroom) {
+        return { error: "Room does not exsits", status: 404 };
+    } else {
+        if (exsistingroom.players.length >= 4) {
+            return { error: "Room is full", status: 403 };
+        }
+        if (exsistingroom.status === "playing") {
+            return { error: "Game already started", status: 403 };
+        }
+        // This check is returning 409, but maybe user IS already in room
+        if (exsistingroom.players.find((player) => player.email === email)) {
+            // Instead of error, return success since the user is already in,
+            return { message: "User already in the room", status: 200 };
+        }
+        const res = await fetchprofilepic(email);
+        exsistingroom.players.push({
+            username,
+            profilepic: res.profilepic,
+            email,
+            role: "player",
+        });
+        await exsistingroom.save();
+        return { message: "joined the room", status: 200 };
     }
-    if (exsistingroom.status === "playing") {
-      return { error: "Game already started", status: 403 };
-    } 
-    if (exsistingroom.players.find(player => player.email === email)) {
-      return { error: "User already in the room", status: 409 };
-    }
-    const res = await fetchprofilepic(email);
-    exsistingroom.players.push({
-      username,
-      profilepic: res.profilepic,
-      email,
-      role: "player"
-    });
-    await exsistingroom.save();
-    return { message: "joined thr room", status: 200 };
-  }
-}
+};
 
 export const leaveroombackend = async (roomid, email) => {
-  await connectDB();
-  const existingRoom = await Room.findOne({ roomID: roomid });
+    await connectDB();
+    const existingRoom = await Room.findOne({ roomID: roomid });
 
-  if (!existingRoom) {
-    return { error: "Room does not exist", status: 404 };
-  }
-  const playerLeaving = existingRoom.players.find(p => p.email === email);
+    if (!existingRoom) {
+        return { error: "Room does not exist", status: 404 };
+    }
+    const playerLeaving = existingRoom.players.find((p) => p.email === email);
 
-  if (!playerLeaving) {
-    return { error: "Player not found in this room", status: 404 };
-  }
-  const updatedRoom = await Room.findOneAndUpdate(
-    { roomID: roomid },
-    { $pull: { players: { email: email } } },
-    { new: true }
-  );
-  if (updatedRoom.players.length === 0) {
-    await Room.deleteOne({ roomID: roomid });
-    return { message: "Player removed and empty room was deleted.", status: 200 };
-  }
-  if (playerLeaving.role === 'host') {
-    await Room.deleteOne({ roomID: roomid });
-    return { message: "Host left the room.", status: 201 };
-  }
-  return { message: "Player removed successfully.", status: 200,room: updatedRoom};
+    if (!playerLeaving) {
+        return { error: "Player not found in this room", status: 404 };
+    }
+    const updatedRoom = await Room.findOneAndUpdate(
+        { roomID: roomid },
+        { $pull: { players: { email: email } } },
+        { new: true }
+    );
+    if (updatedRoom.players.length === 0) {
+        await Room.deleteOne({ roomID: roomid });
+        return {
+            message: "Player removed and empty room was deleted.",
+            status: 200,
+        };
+    }
+    if (playerLeaving.role === "host") {
+        await Room.deleteOne({ roomID: roomid });
+        return { message: "Host left the room.", status: 201 };
+    }
+    return {
+        message: "Player removed successfully.",
+        status: 200,
+        room: updatedRoom,
+    };
 };
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,12 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 // import leaveroombackend from "../app/action/room.js";
 
-
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
-    }
+        methods: ["GET", "POST"],
+    },
 });
 
 httpServer.listen(4000, () => {
@@ -25,49 +24,69 @@ io.on("connection", (socket) => {
         if (socketinroom.size > 0) {
             const room = socket.rooms[0];
             // socket.leave(room);
-            
-            console.log("✅", email, "left the room with id" + room, "upon disconnection");
-        }
 
-    })
-    socket.on("join-room", (roomID, username, email) => {
-        console.log(`✅${username} is trying to join room with id ${roomID} and his email is ${email}`);
+            console.log(
+                "✅",
+                email,
+                "left the room with id" + room,
+                "upon disconnection"
+            );
+        }
+    });
+    socket.on("join-room", async (roomID, username, email) => {
+        console.log(
+            `✅${username} is trying to join room with id ${roomID} and his email is ${email}`
+        );
         try {
             const myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
 
             const raw = JSON.stringify({
-                "roomId": roomID,
-                "email": email,
-                "username": username
+                roomId: roomID,
+                email: email,
+                username: username,
             });
 
             const requestOptions = {
                 method: "POST",
                 headers: myHeaders,
                 body: raw,
-                redirect: "follow"
+                redirect: "follow",
             };
 
-            const res = fetch("http://localhost:3000/api/room/join", requestOptions)
+            const res = await fetch(
+                "http://localhost:3000/api/room/join",
+                requestOptions
+            );
             if (res.status === 200) {
                 socket.join(roomID);
                 io.to(roomID).emit("user_joined", {
                     user: username,
-                    message: "has joined the room"
+                    message: "has joined the room",
                 });
                 socket.emit("joined-room", (roomID, username, email));
+            } else {
+                // TODO: fix this log to be displayed unnecessarily
+                socket
+                    .to(roomID)
+                    .emit(username, "was unable to connect to the room");
+                console.log(
+                    username,
+                    "tried to connect with room id",
+                    roomID,
+                    "but was unable to connect",
+                    res.message,
+                    res.status,
+                    res.error,
+                    res
+                );
             }
-            else {
-                socket.to(roomID).emit(username, "was unable to connect to the room");
-                console.log(username, "tried to connect with room id", roomID, "but was unable to connect");
-            }
-
-        } catch (err) {
-        }
-    })
+        } catch (err) {}
+    });
     socket.on("leave-room", async (roomID, email) => {
-        console.log(`❌ ${email} left room: ${roomID} with socket ID: ${socket.id}`);
+        console.log(
+            `❌ ${email} left room: ${roomID} with socket ID: ${socket.id}`
+        );
 
         try {
             const myHeaders = new Headers();
@@ -75,39 +94,44 @@ io.on("connection", (socket) => {
 
             const raw = JSON.stringify({
                 roomId: roomID,
-                email: email
+                email: email,
             });
 
             const requestOptions = {
                 method: "POST",
                 headers: myHeaders,
                 body: raw,
-                redirect: "follow"
+                redirect: "follow",
             };
 
-            const res = await fetch("http://localhost:3000/api/room/leave", requestOptions);
+            const res = await fetch(
+                "http://localhost:3000/api/room/leave",
+                requestOptions
+            );
             const data = await res.json();
 
             if (res.status === 200) {
-
                 socket.leave(roomID);
-                io.to(roomID).emit("player-left", { email, message: data.message });
+                io.to(roomID).emit("player-left", {
+                    email,
+                    message: data.message,
+                });
 
-                if (data.message && data.message.includes("empty room was deleted")) {
-
+                if (
+                    data.message &&
+                    data.message.includes("empty room was deleted")
+                ) {
                     io.in(roomID).emit("room-deleted", { reason: "empty" });
                     io.in(roomID).socketsLeave(roomID);
                 }
             } else if (res.status === 201) {
-
                 io.to(roomID).emit("room-deleted", { reason: "host-left" });
                 io.in(roomID).socketsLeave(roomID);
             } else {
-
                 socket.emit("error-leaving-room", {
                     error: data.error || "Some Error occured at our side",
                     roomID,
-                    email
+                    email,
                 });
             }
         } catch (error) {
@@ -115,15 +139,16 @@ io.on("connection", (socket) => {
             socket.emit("error-leaving-room", {
                 error: "Server error processing leave request",
                 roomID,
-                email
+                email,
             });
         }
-    })
+    });
 
     socket.on("create-room", (roomID, username) => {
         socket.join(roomID);
-        console.log(`✅ ${username} create room: ${roomID} with socket ID: ${socket.id}`);
+        console.log(
+            `✅ ${username} create room: ${roomID} with socket ID: ${socket.id}`
+        );
         // socket.to(roomID).emit(username, "has joined the room");
-    })
-
-})
+    });
+});
